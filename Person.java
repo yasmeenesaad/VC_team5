b14927a1919b75bcd4a2b17b425b5eff791acc3b
jasmine saad <yasmeenesaad@gmail.com>
2024-07-26 14:28:16 +0300

    classes added

--- Person.java
+++ Person.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class person {
+public class Person {
+    String name;
+    int ID;
+    String Email;
+
+    public Person(String name, int ID, String email) {
+        this.name = name;
+        this.ID = ID;
+        Email = email;
+    }
+
+    public Person() {
+        this.name = "no name";
+        this.ID =0 ;
+        Email = "no email";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
 }
--- Student.java
+++ Student.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Student {
+public class Student  extends Person{
+    float grades;
+    String Stage;
+
+    public Student(String name, int ID, String email, float grades, String stage) {
+        super(name, ID, email);
+        this.grades = grades;
+        Stage = stage;
+    }
+
+    public Student(float grades, String stage) {
+        this.grades = grades;
+        Stage = stage;
+    }
+
+    public float getGrades() {
+        return grades;
+    }
+
+    public void setGrades(float grades) {
+        this.grades = grades;
+    }
+
+    public String getStage() {
+        return Stage;
+    }
+
+    public void setStage(String stage) {
+        Stage = stage;
+    }
 }
