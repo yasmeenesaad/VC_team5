54bbdb23bfaed90ef01569fca2372a3d37a6f8ca
topmax42 <topmax42@gmail.com>
2024-07-26 14:44:58 +0300

    feet_to_cm

--- Algos.java
+++ Algos.java
@@ -6,6 +6,9 @@ public class Algos {
 
     public void convertFeetCm(int x){
 
+        System.out.println(x+" cm = "+ x*30.48+" inches");
+	x= (int)( x*30.48);
+
     }
 
     public void convertInchCm(int x){
